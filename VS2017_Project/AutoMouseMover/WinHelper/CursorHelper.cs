ce6fc97b2e781212e80071099cf794db7318e275
marufdolani <2165276+marufdolani@users.noreply.github.com>
2024-01-26 16:27:08 -0800

    Added support for multiple monitors

--- VS2017_Project/AutoMouseMover/WinHelper/CursorHelper.cs
+++ VS2017_Project/AutoMouseMover/WinHelper/CursorHelper.cs
@@ -51,23 +51,20 @@ namespace AutoMouseMover.WinHelper
         // Set cursor position absolute
         public static bool SetPositionAbsolute(CursorPosition cPos)
         {
-            // Get resolution
-            var res_info = DesktopHelper.GetResolution();
+            Point cursorPos = new Point(cPos.X, cPos.Y);
+            Rectangle monitorBounds = GetMonitorBounds(cursorPos);
 
-            // Absolute coordinates are from 0 to 65536
-            int real_x = (cPos.X * 65536) / res_info.Width;
-            int real_y = (cPos.Y * 65536) / res_info.Height;
+            int real_x = ((cPos.X - monitorBounds.Left) * 65536) / monitorBounds.Width;
+            int real_y = ((cPos.Y - monitorBounds.Top) * 65536) / monitorBounds.Height;
 
-            // Create INPUT structure
             var input = new SendInputWrapper.Input();
-            // Fill it
-            input.mType                = SendInputWrapper.eInputTypes.INPUT_MOUSE;
+            input.mType = SendInputWrapper.eInputTypes.INPUT_MOUSE;
             input.mData.mMi.mMouseData = 0;
-            input.mData.mMi.mTime      = 0;
-            input.mData.mMi.mX         = real_x;
-            input.mData.mMi.mY         = real_y;
-            input.mData.mMi.mFlags     = SendInputWrapper.eMouseEventFlags.MOUSEEVENTF_ABSOLUTE | SendInputWrapper.eMouseEventFlags.MOUSEEVENTF_MOVE;
-            // Send input
+            input.mData.mMi.mTime = 0;
+            input.mData.mMi.mX = real_x;
+            input.mData.mMi.mY = real_y;
+            input.mData.mMi.mFlags = SendInputWrapper.eMouseEventFlags.MOUSEEVENTF_ABSOLUTE | SendInputWrapper.eMouseEventFlags.MOUSEEVENTF_MOVE;
+
             return SendInputWrapper.SendInput(input);
         }
 
@@ -87,13 +84,20 @@ namespace AutoMouseMover.WinHelper
             // Send input
             return SendInputWrapper.SendInput(input);
         }
-
+        // Get the bounds of the monitor where the cursor is located
+        public static Rectangle GetMonitorBounds(Point cursorPos)
+        {
+            Screen currentScreen = Screen.FromPoint(cursorPos);
+            return currentScreen.Bounds;
+        }
         // Check absolute position
         public static bool CheckAbsolutePosition(CursorPosition cPos)
         {
-            ResolutionInfo res_info = DesktopHelper.GetResolution();
-            return (((cPos.X >= 0) && (cPos.X <= res_info.Width )) && 
-                    ((cPos.Y >= 0) && (cPos.Y <= res_info.Height)));
+            Point cursorPos = new Point(cPos.X, cPos.Y);
+            Rectangle monitorBounds = GetMonitorBounds(cursorPos);
+        
+            return (cursorPos.X >= monitorBounds.Left && cursorPos.X <= monitorBounds.Right &&
+                    cursorPos.Y >= monitorBounds.Top && cursorPos.Y <= monitorBounds.Bottom);
         }
 
         // Check relative position
